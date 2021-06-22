097920f09e586b9c070b0f551803530591ba71a3
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-06-22 11:58:32 +0100

    Added basic CSS

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,7 +2,8 @@ function PageHeader(): JSX.Element {
     return (
       <header>
         <h1>Akwasi's Favourite Places</h1>
-        <p>My fav's</p>
+        
+        <iframe title = "Akwasi's song" src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="80" allowTransparency= {false} frameBorder="0" allow="encrypted-media"></iframe>
       </header>
     );
   }
